31b1606d72594301255976d978b542c655e760cd
dima <d.ananyev@a1qa.com>
2020-10-06 17:19:03 +0400

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(expr, bracketsConfig){
+    const holder = []
+    const openBrackets = ['(','{','[']
+    const closedBrackets = [')','}',']']
+    for (let letter of (expr + bracketsConfig.toString())) {
+        if(openBrackets.includes(letter)){
+            holder.push(letter)
+        } else if(closedBrackets.includes(letter)){
+            const openPair = openBrackets[closedBrackets.indexOf(letter)]
+            if(holder[holder.length - 1] === openPair){
+                holder.splice(-1,1)
+            }else{
+                holder.push(letter)
+                break
+            }
+          }
+    }
+    return (holder.length === 0) // return true if length is 0, otherwise false
 }
